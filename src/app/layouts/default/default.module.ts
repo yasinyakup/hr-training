81313bea5e19e98c75311f85a8744a7877463f18
yasinyakup <yasinyakup@gmail.com>
2020-12-28 08:30:54 +0300

    routers setted

---
+++ src/app/layouts/default/default.module.ts
@@ -0,0 +1,27 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+
+import { DashboardComponent } from 'src/app/modules/dashboard/dashboard.component';
+import { RouterModule } from '@angular/router';
+import { DefaultComponent } from './default.component';
+import { SharedModule } from 'src/app/shared/shared.module';
+import { PostsComponent } from 'src/app/modules/posts/posts.component';
+import { MatSidenavModule } from '@angular/material/sidenav'
+
+
+@NgModule({
+  declarations: [
+    DefaultComponent,
+    DashboardComponent,
+    PostsComponent
+  ],
+  imports: [
+  CommonModule,
+  RouterModule,
+  SharedModule,
+  MatSidenavModule
+
+  ]
+})
+export class DefaultModule { }
---
+++ src/app/shared/shared.module.ts
@@ -0,0 +1,23 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { SidebarComponent } from './components/sidebar/sidebar.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { PostsComponent } from '../modules/posts/posts.component';
+import { HeaderComponent } from './components/header/header.component';
+
+
+
+@NgModule({
+  declarations: [SidebarComponent,
+    FooterComponent,
+    HeaderComponent],
+  imports: [
+    CommonModule
+  ],
+  exports:[
+    SidebarComponent,
+    FooterComponent,
+    HeaderComponent
+  ]
+})
+export class SharedModule { }
