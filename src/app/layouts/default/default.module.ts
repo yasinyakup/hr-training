f24a1e8c12060fe0c0a4beaf536d5f62ba4b6d73
yasinyakup <yasinyakup@gmail.com>
2020-12-29 09:46:39 +0300

    mat table and sorting functions

--- src/app/layouts/default/default.module.ts
+++ src/app/layouts/default/default.module.ts
@@ -14,6 +14,9 @@ import {MatCardModule} from '@angular/material/card'
 import { DashboardService } from 'src/app/modules/dashboard.service';
 import {MatPaginatorModule} from '@angular/material/paginator'
 import {MatTableModule} from '@angular/material/table'
+import { MatSortModule } from '@angular/material/sort';
+import { MatInputModule } from '@angular/material/input';
+import { MatFormFieldModule } from '@angular/material/form-field';
 
 
 @NgModule({
@@ -31,7 +34,10 @@ import {MatTableModule} from '@angular/material/table'
   FlexLayoutModule,
   MatCardModule,
   MatTableModule,
-  MatPaginatorModule
+  MatPaginatorModule,
+  MatSortModule,
+  MatInputModule,
+  MatFormFieldModule
   ],
   providers:[
     DashboardService
--- src/app/modules/posts/posts.component.ts
+++ src/app/modules/posts/posts.component.ts
@@ -1,4 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { MatPaginator } from '@angular/material/paginator';
+import { MatSort } from '@angular/material/sort';
 import { MatTableDataSource } from '@angular/material/table';
 
 interface IUser {
@@ -6,7 +9,7 @@ interface IUser {
   name: string;
   age: number;
   title: string;
-  hireDate: Date;
+  hireDate: string;
   dept: string;
 
 
@@ -22,6 +25,9 @@ export class PostsComponent implements OnInit {
  dataSource: MatTableDataSource<IUser>;
   users: IUser[];
   displayedColumns: String[]=['id','name', 'age', 'title', 'hireDate', 'dept'];
+  @ViewChild(MatSort, { static: true })  sort: MatSort = new MatSort;
+  @ViewChild(MatPaginator, {static: true}) paginator: MatPaginator;
+
   constructor() { 
     this.users = [
       {
@@ -29,7 +35,7 @@ export class PostsComponent implements OnInit {
         name: 'yasin yakup',
         age: 36,
         title:'engineer',
-        hireDate: new Date('2020-03-15'),
+        hireDate: '2020-01-15', 
         dept: "IT"
       },
       {
@@ -37,7 +43,7 @@ export class PostsComponent implements OnInit {
         name: 'malih yayla',
         age: 27,
         title:'engineer',
-        hireDate: new Date('2020-01-15'),
+        hireDate: '2020-03-15',
         dept: "IT"
       },
       {
@@ -45,7 +51,31 @@ export class PostsComponent implements OnInit {
         name: 'bilal abdurahman',
         age: 30,
         title:'engineer',
-        hireDate: new Date('2020-09-15'),
+        hireDate: '2020-09-05',
+        dept: "IT"
+      },
+      {
+        id:'4',
+        name: 'bilal abdurahman',
+        age: 30,
+        title:'engineer',
+        hireDate: '2020-09-05',
+        dept: "IT"
+      },
+      {
+        id:'5',
+        name: 'bilal abdurahman',
+        age: 30,
+        title:'engineer',
+        hireDate: '2020-09-05',
+        dept: "IT"
+      },
+      {
+        id:'6',
+        name: 'bilal abdurahman',
+        age: 30,
+        title:'engineer',
+        hireDate: '2020-09-05',
         dept: "IT"
       }
     ];
@@ -54,6 +84,13 @@ this.dataSource = new MatTableDataSource<IUser>(this.users);
   }
 
   ngOnInit(): void {
+    this.dataSource.sort = this.sort;
+    this.dataSource.paginator = this.paginator;
   }
 
+   WithoutTime(dateTime: Date) {
+    var date = new Date(dateTime.getTime());
+    date.setHours(0, 0, 0, 0);
+    return date;
+}
 }
